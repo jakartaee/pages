77eed2e2e3e7e393a31222f18fc86412243aaaa6
Mark Thomas <markt@apache.org>
2021-09-23 21:06:08 +0100

    Fix #40. Option to trigger an exception for an unknown EL identifier

--- api/src/main/java/jakarta/servlet/jsp/el/NotFoundELResolver.java
+++ api/src/main/java/jakarta/servlet/jsp/el/NotFoundELResolver.java
@@ -17,11 +17,13 @@ package jakarta.servlet.jsp.el;
 
 import java.beans.FeatureDescriptor;
 import java.util.Iterator;
+import java.util.ResourceBundle;
 import java.util.Collections;
 
 import jakarta.el.ELContext;
 import jakarta.el.ELResolver;
 import jakarta.el.ELException;
+import jakarta.el.PropertyNotFoundException;
 
 /**
  * Defines variable resolution when all other resolvers fail.
@@ -29,6 +31,9 @@ import jakarta.el.ELException;
  * @since JSP 3.1
  */
 public class NotFoundELResolver extends ELResolver {
+
+    private static final String LSTRING_FILE = "jakarta.servlet.jsp.LocalStrings";
+    private static final ResourceBundle lStrings = ResourceBundle.getBundle(LSTRING_FILE);
 
     /**
      * If the base object is <code>null</code>, searches the Class and static imports for an import with the given name
@@ -44,6 +49,11 @@ public class NotFoundELResolver extends ELResolver {
      * @param property Ignored
      * @return Always {@code null}
      * @throws NullPointerException if context is <code>null</code>
+     * @throws PropertyNotFoundException
+     *                              If the provided context contains a Boolean object with value {@code Boolean.TRUE} as
+     *                              the value associated with the key
+     *                              {@code jakarta.servlet.jsp.el.NotFoundELResolver.class}. This is to support
+     *                              implementation of the {@code errorOnELNotFound} page/tag directive.
      * @throws ELException          if an exception was thrown while performing the property or variable resolution. The
      *                              thrown exception must be included as the cause property of this exception, if
      *                              available.
@@ -55,6 +65,12 @@ public class NotFoundELResolver extends ELResolver {
             throw new NullPointerException();
         }
 
+        Object obj = context.getContext(this.getClass());
+        if (obj instanceof Boolean && ((Boolean) obj).booleanValue()) {
+            throw new PropertyNotFoundException(
+                    lStrings.getString("el.unknown.identifier") + " [" + property.toString() + "]");
+        }
+        
         context.setPropertyResolved(true);
 
         return null;
