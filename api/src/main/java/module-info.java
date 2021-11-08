ba30062d35c62b5057f63569db92bb84b4a9976b
Mark Thomas <markt@apache.org>
2021-11-08 21:49:53 +0000

    Add module-info and remove automatic module name from manifest

---
+++ api/src/main/java/module-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+module jakarta.servlet.jsp {
+    exports jakarta.servlet.jsp;
+    exports jakarta.servlet.jsp.tagext;
+    exports jakarta.servlet.jsp.el;
+
+    requires transitive jakarta.el;
+    requires transitive jakarta.servlet.api;
+    requires transitive java.desktop;
+}
